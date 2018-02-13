96759174ff1635cc652bca18bf21e5ce01cc2bc6
Margherita Pecorelli <margherita.pecorelli@studio.unibo.it>
2018-02-13 12:05:10 +0100

    better version of f5 - with partial gui for data history request

---
+++ src/it/unibo/qpatient_data_retriever/CustomGUISupportPatient.java
@@ -0,0 +1,73 @@
+package it.unibo.qpatient_data_retriever;
+
+import java.awt.Color;
+import java.awt.Label;
+import java.awt.Panel;
+
+import it.unibo.is.interfaces.IActivityBase;
+import it.unibo.system.SituatedPlainObject;
+import it.unibo.is.interfaces.IBasicEnvAwt;
+import it.unibo.qactors.QActorContext;
+import it.unibo.qactors.QActorUtils;
+
+public class CustomGUISupportPatient extends SituatedPlainObject {
+
+	private IActivityBase cmdHandler;
+	private IBasicEnvAwt envAwt;
+	private QActorContext ctx;
+	
+	public CustomGUISupportPatient(IBasicEnvAwt env, QActorContext myCtx) {
+		super(env);
+		envAwt = env;
+		init();
+		this.ctx = myCtx;
+	}
+	
+	protected void init(){
+		cmdHandler = new CmdHandler(envAwt);
+		setCommandUI();
+//		setInputUI();
+//		addCustomPanel();	
+	}
+	
+	protected void setCommandUI(){
+		envAwt.addCmdPanel("commandPanel", new String[]{"RETRIEVE DATA"}, cmdHandler);
+	}
+	
+//	protected void setInputUI(){
+//		envAwt.addInputPanel( 10 );
+//	}
+//		
+//	protected void addCustomPanel(){
+//		Panel p = new Panel();
+//		p.setBackground(Color.blue);
+//		Label l = new Label("a label");
+//		l.setBackground(Color.cyan);
+//		p.add(l);
+//		envAwt.addPanel(p);
+//	}
+	
+	public void printOnGUI(String str) {
+		println(str);
+	}
+
+	private class CmdHandler extends SituatedPlainObject implements IActivityBase {
+	
+		public CmdHandler(IBasicEnvAwt env) {
+			super(env);
+		}
+		
+		@Override
+		public void execAction(String cmd) {
+			String input = env.readln();
+			println("CmdHandler -> " + cmd + " input= " + input);
+			try {
+				QActorUtils.raiseEvent(ctx, "input", "patient_data_request", "patient_data_request");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
