d35c95960c29ec24576a30098d113f7d76bf6816
Margherita Pecorelli <margherita.pecorelli@studio.unibo.it>
2018-02-13 16:25:07 +0100

    problem-analysis b5 f6

---
+++ src/it/unibo/qdc_user_manager/CustomGUIRegistration.java
@@ -0,0 +1,85 @@
+package it.unibo.qdc_user_manager;
+
+import java.awt.Color;
+import java.awt.Label;
+import java.awt.Panel;
+
+import it.unibo.is.interfaces.IActivityBase;
+import it.unibo.system.SituatedPlainObject;
+import it.unibo.is.interfaces.IBasicEnvAwt;
+import it.unibo.qactors.QActorContext;
+import it.unibo.qactors.QActorUtils;
+
+public class CustomGUIRegistration extends SituatedPlainObject {
+
+	private IActivityBase cmdHandler;
+	private IBasicEnvAwt envAwt;
+	private QActorContext ctx;
+	
+	public CustomGUIRegistration(IBasicEnvAwt env, QActorContext myCtx) {
+		super(env);
+		envAwt = env;
+		init();
+		this.ctx = myCtx;
+	}
+	
+	protected void init(){
+		cmdHandler = new CmdHandler(envAwt);
+		setCommandUI();
+		setInputUI();
+//		addCustomPanel();	
+	}
+	
+	protected void setCommandUI(){
+		envAwt.addCmdPanel("commandPanel", new String[]{"DOCTOR REGISTRATION", "PATIENT REGISTRATION"}, cmdHandler);
+	}
+	
+	protected void setInputUI(){
+		envAwt.addInputPanel(50);
+	}
+		
+//	protected void addCustomPanel(){
+//		Panel p = new Panel();
+//		p.setBackground(Color.blue);
+//		Label l = new Label("a label");
+//		l.setBackground(Color.cyan);
+//		p.add(l);
+//		envAwt.addPanel(p);
+//	}
+	
+	public void printOnGUI(String str) {
+		println(str);
+	}
+
+	private class CmdHandler extends SituatedPlainObject implements IActivityBase {
+	
+		public CmdHandler(IBasicEnvAwt env) {
+			super(env);
+		}
+		
+		@Override
+		public void execAction(String cmd) {
+			String input = env.readln();
+			println("CmdHandler -> " + cmd + " input= " + input);
+			switch (cmd) {
+			case "DOCTOR REGISTRATION":	
+				try {
+					QActorUtils.raiseEvent(ctx, "input", "register_request", "register_request(" + input + ",D)");
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+			case "PATIENT REGISTRATION":
+				try {
+					QActorUtils.raiseEvent(ctx, "input", "register_request", "register_request(" + input + ",P)");
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+			}
+		}
+	}
+	
+}
---
+++ src/it/unibo/qdoctor_login/CustomGUIDoctorLogin.java
@@ -0,0 +1,76 @@
+package it.unibo.qdoctor_login;
+
+import java.awt.Color;
+import java.awt.Label;
+import java.awt.Panel;
+
+import it.unibo.is.interfaces.IActivityBase;
+import it.unibo.system.SituatedPlainObject;
+import it.unibo.is.interfaces.IBasicEnvAwt;
+import it.unibo.qactors.QActorContext;
+import it.unibo.qactors.QActorUtils;
+
+public class CustomGUIDoctorLogin extends SituatedPlainObject {
+
+	private IActivityBase cmdHandler;
+	private IBasicEnvAwt envAwt;
+	private QActorContext ctx;
+	private String actorId;
+	
+	public CustomGUIDoctorLogin(IBasicEnvAwt env, String actorId, QActorContext myCtx) {
+		super(env);
+		envAwt = env;
+		init();
+		this.ctx = myCtx;
+		this.actorId = actorId;
+	}
+	
+	protected void init(){
+		cmdHandler = new CmdHandler(envAwt);
+		setCommandUI();
+		setInputUI();
+//		addCustomPanel();	
+	}
+	
+	protected void setCommandUI(){
+		envAwt.addCmdPanel("commandPanel", new String[]{"LOGIN"}, cmdHandler);
+	}
+	
+	protected void setInputUI(){
+		envAwt.addInputPanel(50);
+	}
+		
+//	protected void addCustomPanel(){
+//		Panel p = new Panel();
+//		p.setBackground(Color.blue);
+//		Label l = new Label("a label");
+//		l.setBackground(Color.cyan);
+//		p.add(l);
+//		envAwt.addPanel(p);
+//	}
+	
+	public void printOnGUI(String str) {
+		println(str);
+	}
+
+	private class CmdHandler extends SituatedPlainObject implements IActivityBase {
+	
+		public CmdHandler(IBasicEnvAwt env) {
+			super(env);
+		}
+		
+		@Override
+		public void execAction(String cmd) {
+			String input = env.readln();
+			println("CmdHandler -> " + cmd + " input= " + input);
+			String acId = actorId.substring(0, actorId.length()-5);
+			try {
+				QActorUtils.raiseEvent(ctx, "input", "login_request", "login_request(" + input + "," + acId + ")");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
